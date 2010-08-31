1d61f9708cb780bf2efb9acd04c4431043df86ab
deml.tomas@seznam.cz <deml.tomas@seznam.cz@fa22ef35-90ad-4730-0472-c7e665cd12bb>
2010-08-31 17:29:28 +0000

    Refactoring

--- trunk/Roamie/Properties/AssemblyInfo.cs
+++ trunk/Roamie/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // 1.9.2010 = 2010 => 0 | 09 => 09 | 01 => 01 = 00901
 [assembly: AssemblyVersion("0.7.0.000901")]
-[assembly: AssemblyFileVersion("0.7.0.00901")]
+[assembly: AssemblyFileVersion("0.7.0.000901")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
